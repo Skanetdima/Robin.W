62ae9d178c825522d07e2ae1ef504cdfb5f6be14
Dmitry <dimabogacan@gmail.com>
2024-03-02 12:35:22 +0100

    firstContainer completed

--- src/firstContainer.js
+++ src/firstContainer.js
@@ -1,3 +1,15 @@
 export const FirstContainer = ({ className }) => {
-  return <div className={className}>Hello!</div>;
+  return (
+    <div className={className}>
+      <h1 className="heading1">
+        I’m Robin Williams. <br /> A Product Designer <br />
+        <span>based in Italy.</span>
+      </h1>
+      <p className="pargraph">
+        I’m probably the most passionate designer you will ever get to work
+        with. If you have <br /> a great project that needs some amazing skills,
+        I’m your guy.
+      </p>
+    </div>
+  );
 };
