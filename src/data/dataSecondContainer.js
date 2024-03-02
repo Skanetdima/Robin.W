7ab1ae74404d76379fd670131a71307c10c40252
Dmitry <dimabogacan@gmail.com>
2024-03-02 22:57:50 +0100

    Second container completed + condition to fix

---
+++ src/data/dataSecondContainer.js
@@ -0,0 +1,23 @@
+export const dataSecondContainer = [
+  {
+    id: 0,
+    name: ", Interaction Designer",
+    company: "Google",
+    idOfName: "01",
+    text: "I currently am the lead designer on the interaction design team for Google Play.",
+  },
+  {
+    id: 1,
+    name: ", Product Designer",
+    company: "Facebook",
+    idOfName: "02",
+    text: "I’ve worked on a wide variety of internal tools for facebook over the past 6 years.",
+  },
+  {
+    id: 2,
+    name: ", Graphic Designer",
+    company: "Dribble",
+    idOfName: "03",
+    text: "I started my design career with Dribbble. I was incharge of creating illustrations for the platform.",
+  },
+];
--- src/secondContainer.js
+++ src/secondContainer.js
@@ -1,12 +1,32 @@
+import { dataSecondContainer } from "./data/dataSecondContainer";
 export const SecondContainer = ({ className }) => {
-  const divItems = null;
+  const divItems = dataSecondContainer.map((item) => (
+    <div key={item.id}>
+      <span className="label1">{item.idOfName}</span>
+      <h3>
+        <span
+          style={
+            item.company === "Google"
+              ? { color: "green" }
+              : item.company === "Facebook"
+              ? { color: "blue" }
+              : { color: "red" }
+          }
+        >
+          {item.company}
+        </span>
+        {item.name}
+      </h3>
+      <p className="body1">{item.text}</p>
+    </div>
+  ));
   return (
     <div className={className}>
       <span className="overline">work experience</span>
       <h2 className="heading2">
         Companies I have worked <br /> for in the past
       </h2>
-      <div>{divItems}</div>
+      <div className="secondContainerItems">{divItems}</div>
     </div>
   );
 };
