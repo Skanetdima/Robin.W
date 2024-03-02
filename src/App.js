74bfdf7082e805b38e5a3111c3916d8f0027b927
Dmitry <dimabogacan@gmail.com>
2024-03-02 12:43:54 +0100

    second container processing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./styles/style.css";
 import { Header } from "./header";
 import { FirstContainer } from "./firstContainer";
+import { SecondContainer } from "./secondContainer";
 export default function App() {
   return (
     <>
       <Header className="header" />
       <main>
         <FirstContainer className="firstContainer" />
+        <SecondContainer className="secondContainer" />
       </main>
     </>
   );
---
+++ src/secondContainer.js
@@ -0,0 +1,12 @@
+export const SecondContainer = ({ className }) => {
+  const divItems = null;
+  return (
+    <div className={className}>
+      <span className="overline">work experience</span>
+      <h2 className="heading2">
+        Companies I have worked <br /> for in the past
+      </h2>
+      <div>{divItems}</div>
+    </div>
+  );
+};
