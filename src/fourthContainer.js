ef037496085cb8561e9d7d9960d23d0f7f1d6b94
Dmitry <dimabogacan@gmail.com>
2024-03-03 18:03:23 +0100

    fourthContainer not margined

--- src/fourthContainer.js
+++ src/fourthContainer.js
@@ -4,12 +4,19 @@ export const FourthContainer = ({ className }) => {
     <li key={object.id}>
       <img src={object.image} />
       <h3 className="heading3">{object.name}</h3>
-      <p className="pargraph">{object.text}</p>
+      <p className="body2">{object.text}</p>
     </li>
   ));
   return (
     <div className={className}>
-      <div className="fourthContainerHeaderContainer"></div>
+      <div className="fourthContainerHeaderContainer">
+        <h2 className="heading2">Skillset</h2>
+        <p className="pargraph">
+          With skills in over 4 different fields of design, I am the perfect
+          person to hire when it comes to a full fledged project. Whatever your
+          needs are, I can pretty much take on any challenge.
+        </p>
+      </div>
       <ul className="fourthContainerItems">{divItems}</ul>
     </div>
   );
