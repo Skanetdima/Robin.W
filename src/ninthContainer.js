39420c32258ca7450c3ef96556281352afa43c49
Dmitry <dimabogacan@gmail.com>
2024-03-04 20:49:06 +0100

    ninthContainer completed + aligning items properly

--- src/ninthContainer.js
+++ src/ninthContainer.js
@@ -3,14 +3,23 @@ export const NinthContainer = ({ className }) => {
   const divItems = dataNinthContainer.map((object) => (
     <li key={object.id}>
       <img src={object.image} />
-      <h3>{object.text}</h3>
-      <p>{object.name}</p>
-      <span>{object.company}</span>
+      <div>
+        <h3 className="heading3">{object.text}</h3>
+        <h4>{object.name}</h4>
+        <span>{object.company}</span>
+      </div>
     </li>
   ));
   return (
     <div className={className}>
-      <div></div>
+      <div>
+        <h2 className="heading2">This is what people say about me</h2>
+        <p>
+          Here are a few lines from people who I have worked with over the past
+          8+ years in my design career.
+        </p>
+        <a href="#">See all testimonials</a>
+      </div>
       <ul>{divItems}</ul>
     </div>
   );
