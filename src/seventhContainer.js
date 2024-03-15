d103c155cad9f5f07a4b2463669c61388fd66b7c
Dmitry <dimabogacan@gmail.com>
2024-03-15 20:14:04 +0100

    added optimization #1

--- src/seventhContainer.js
+++ src/seventhContainer.js
@@ -1,13 +1,16 @@
 export const SeventhContainer = ({ className }) => {
   return (
     <div className={className}>
-      <h2 className="heading2">Instagram</h2>
-      <p className="pargraph">
-        If you area a person who enjoys photography, then I highly recommend
-        that you check out my Instagram. I’m an avid traveller and I capture the
-        best moments that I would love to cherish with the world
-      </p>
-      <a href="#">Follow me on IG</a>
+      <div className="seventhContainerItem">
+        <h2 className="heading2">Instagram</h2>
+        <p className="pargraph">
+          If you area a person who enjoys photography, then I highly recommend
+          that you check out my Instagram. I’m an avid traveller and I capture
+          the best moments that I would love to cherish with the world
+        </p>
+        <a href="#">Follow me on IG</a>
+      </div>
+      <div className="seventhContainerItem"></div>
     </div>
   );
 };
